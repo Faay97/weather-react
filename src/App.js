80c99ce947412fa35533736dbe0b04f0eb34f3bd
Faay97 <101605402+Faay97@users.noreply.github.com>
2022-06-01 21:13:02 -0400

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,19 +5,21 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
-      <Weather city="Toronto" />
-      <footer>
-        This project was coded by Favour Umoru and is open-sourced on
-        <a
-          href="https://github.com/Faay97/weather-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Hello World</h1>
+        <Weather city="Toronto" />
+        <footer>
+          This project was coded by Favour Umoru and is open-sourced on
+          <a
+            href="https://github.com/Faay97/weather-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
